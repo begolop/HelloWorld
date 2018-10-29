b36f37308c04d7ed5ccd181f5f6032f78af4b46e
admin <admin@Win7_1TB>
2018-10-29 09:20:23 +0100

    He añadido display

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -11,11 +11,10 @@ package helloworld;
  */
 public class Helloworld {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
